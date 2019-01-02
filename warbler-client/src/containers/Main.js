a8888465f7a850b41e4d379a7378481c9cdfa737
Benedict Benida <clyd.benidz@gmail.com>
2019-01-02 22:18:44 +0800

    applied user authentication

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
+import { authUser } from "../store/actions/auth";
 
 const Main = props => {
+  const {authUser} = props;
   return (
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props}/>} />
+        <Route exact path="/signin" render={props => (
+          <AuthForm onAuth={authUser} buttonText="Login" heading="Welcome Back!" {...props}/>
+        )} />
+        <Route exact path="/signup" render={props => (
+          <AuthForm onAuth={authUser} signUp buttonText="Sign me up" heading="Join Warbler today!" {...props}/>
+        )} />
       </Switch>
     </div>
   )
@@ -19,4 +28,4 @@ function mapStateToProps(reduxState) {
   }
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, {authUser})(Main));
